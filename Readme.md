c8a058b0d6468759a48bdea5eb7957e4b03d66f5
theo <theo@jlab.org>
2024-03-08 16:57:45 -0500

    add more fields and a special service to assign lognumbers without relying on the serial module which is still in beta and probably overkill anyhow

--- Readme.md
+++ Readme.md
@@ -8,8 +8,15 @@ vendor/bin/drush en elog_core
 vendor/bin/drush pmu elog_core
 ```
 
+For some reason the uninstall won't remove tags even though it will remove logbooks.
+We have to do it with drush
+```shell
+vendor/bin/drush config:delete taxonomy.vocabulary.tags
+vendor/bin/drush config:delete field.storage.node.field_tags
+```
 
 ## References
 
 [https://www.digitalnadeem.com/drupal/how-to-create-additional-fields-programmatically-in-user-account-and-displaying-same-in-user-registration-page-drupal-9-and-drupal-10/]()
 
+[https://www.digitalnadeem.com/drupal/how-to-create-custom-field-in-drupal-9/]()
---
+++ src/LogNumberService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\elog_core;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Service for issuing log numbers
+ *
+ */
+class LogNumberService {
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs a LogNumberService object.
+   *
+   * @param LoggerChannelFactory $logger
+   *   A logger instance.
+   */
+  public function __construct(LoggerChannelFactory $loggerFactory) {
+    $this->logger = $loggerFactory->get('elog');
+  }
+
+  public function nextLogNumber() {
+    $connection = Database::getConnection();
+    // @todo review https://api.drupal.org/api/drupal/core%21includes%21database.inc/function/db_transaction/8.2.x
+    $transaction = $connection->startTransaction();
+    $this->logger->info('howdy from nextLogNumber');
+    try {
+      // Insert a temporary record to get a new unique serial value.
+      $log_number = $connection->insert('lognumber_sequence')
+        ->fields(['log_number' => NULL])
+        ->execute();
+
+      // If there's a reason why it's come back undefined, reset it.
+      $log_number = $log_number ?? 0;
+
+      // Delete the temporary record.
+      $connection->delete('lognumber_sequence')
+        ->condition('log_number', $log_number, '=')
+        ->execute();
+      // No transaction commit?  I guess it happens when transaction goes out of scope?
+
+    }
+    catch (\Exception $e) {
+      $transaction->rollback();
+      $this->logger->error($e);
+      throw $e;
+    }
+
+    // Return the new unique serial value.
+    return $log_number;
+  }
+
+  public static function create(ContainerInterface $container) {
+    // TODO: Implement create() method.
+  }
+
+}
