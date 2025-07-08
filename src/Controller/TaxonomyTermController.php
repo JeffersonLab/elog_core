524ab34885141e93402c7037576a185b6e0d8ffc
Theo Larrieu <theo@jlab.org>
2025-07-08 16:01:38 -0400

    inheriting from BaseController, it works for tags and logbooks.

--- src/Controller/TaxonomyTermController.php
+++ src/Controller/TaxonomyTermController.php
@@ -3,6 +3,8 @@
 namespace Drupal\elog_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\elog_core\LogentrySqlQuery;
+use Drupal\elog_core\LogentryTabulator;
 use Symfony\Component\HttpFoundation\Request;
 
 class TaxonomyTermController extends ControllerBase{
@@ -11,13 +13,40 @@ class TaxonomyTermController extends ControllerBase{
    * Display the entries of a single logbook.
    */
   public function handle(Request $request) {
+
+    // If the vocabulary isn't tags or logbooks, we'll just let our
+    // parent class handle it.
+    $term = $request->get('taxonomy_term');
+    $vocabulary = $term->get('vid')->first()->target_id;
+    if (! in_array($vocabulary, ['tags','logbooks'])) {
+      $listing['content'] = [
+        '#type' => 'markup',
+        '#markup' => "<p>{$vocabulary->get('name')->value}</p>",
+      ];
+
+      return $listing;
+    }
+
+    dpm($request);
     dpm($request->get('taxonomy_term'));
 
-    $listing['content'] = [
-      '#type' => 'markup',
-      '#markup' => '<p>The taxonomy term code goes here.</p>',
-    ];
+    $query = LogentrySqlQuery::fromRequest($request);
 
-    return $listing;
+    // Explicitly set the logbook or tag to match the taxonomy term
+    // route being viewed.
+    if ($vocabulary == 'logbooks') {
+      $query->setLogbook($term->get('name')->value);
+    }
+    if ($vocabulary == 'tags') {
+      $query->setTag($term->get('name')->value);
+    }
+
+    $entries = $query->resultNodes();
+    dpm($query->__toString());
+    $tabulator = new LogentryTabulator($entries);
+    $tabulator->groupBy = $request->get('groupBy', 'SHIFT');
+    return $tabulator->table();
+
+
   }
 }
